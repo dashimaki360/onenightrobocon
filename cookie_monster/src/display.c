dc7ff730b8594c77b02201899ff18b4e117b62e1
YAMAGUCHI_TAKUYA <yamaguchitakuya@takuya-MBA.local>
2017-02-16 10:15:16 +0900

    add display mode

--- cookie_monster/src/display.c
+++ cookie_monster/src/display.c
@@ -1,7 +1,28 @@
 #include "ev3api.h"
+#include "inc/main.h"
 #include "inc/display.h"
 
+static const float delta_t = 0.001;
+static const float st_p = 5;
+static const float st_i = 0;
+static const float st_d = 0;
+static float lasterror = 0, integral = 0;
+static float midpoint = (20 - 0) / 2 + 0;
+
 void disp_run(void){
+	//float error = midpoint - ev3_color_sensor_get_reflect(line_sensor);
+	float error = midpoint - ev3_color_sensor_get_ambient(color_sensor);
+	error = error;
+	integral += error * delta_t;
+	float steer = st_p * error + st_i * integral + st_d * (error - lasterror)/delta_t;
+	//debug
+	char msg[128] = {};
+	sprintf(msg, "err %4f steer %4f inte %4f", error, steer, integral);
+	ev3_lcd_draw_string(msg,0,40);
+
+	ev3_motor_steer(left_motor, right_motor, -40, steer);
+	lasterror = error;
+	tslp_tsk(1);
   return;
 }
 
